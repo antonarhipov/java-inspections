19e99b7b2cbb551bf32c2f50a70ded3fd1f51e55
anton <ant.arhipov@gmail.com>
2024-02-29 15:10:39 +0200

    Example for locating duplicates

---
+++ src/main/java/org/example/duplicates/DuplicateB.java
@@ -0,0 +1,81 @@
+package org.example.duplicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicateB {
+
+    public static int orientation(Coordinate p, Coordinate q, Coordinate r)
+    {
+        int val = (q.y - p.y) * (r.x - q.x) -
+                  (q.x - p.x) * (r.y - q.y);
+
+        if (val == 0) return 0;  // collinear
+        return (val > 0)? 1: 2; // clock or counterclock wise
+    }
+
+    // Prints convex hull of a set of n points.
+    public static void convexHull(Coordinate points[], int n)
+    {
+        // There must be at least 3 points
+        if (n < 3) return;
+
+        // Initialize Result
+        List<Coordinate> hull = new ArrayList<>();
+
+        // Find the leftmost point
+        int l = 0;
+        for (int i = 1; i < n; i++)
+            if (points[i].x < points[l].x)
+                l = i;
+
+        // Start from leftmost point, keep moving
+        // counterclockwise until reach the start point
+        // again. This loop runs O(h) times where h is
+        // number of points in result or output.
+        int p = l, q;
+        do
+        {
+            // Add current point to result
+            hull.add(points[p]);
+
+            // Search for a point 'q' such that
+            // orientation(p, q, x) is counterclockwise
+            // for all points 'x'. The idea is to keep
+            // track of last visited most counterclock-
+            // wise point in q. If any point 'i' is more
+            // counterclock-wise than q, then update q.
+            q = (p + 1) % n;
+
+            for (int i = 0; i < n; i++)
+            {
+                // If i is more counterclockwise than
+                // current q, then update q
+                if (orientation(points[p], points[i], points[q])
+                    == 2)
+                    q = i;
+            }
+
+            // Now q is the most counterclockwise with
+            // respect to p. Set p as q for next iteration,
+            // so that q is added to result 'hull'
+            p = q;
+
+        } while (p != l);  // While we don't come to first
+        // point
+
+        // Print Result
+        for (Coordinate temp : hull)
+            System.out.println("(" + temp.x + ", " +
+                               temp.y + ")");
+    }
+}
+
+class Coordinate
+{
+    int x, y;
+    Coordinate(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+}
